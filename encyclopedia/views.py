6d467ab6c4c86f15fe398c08b4d42eeb4cab0086
danielkanangila <daniel.kanangila@gmail.com>
2020-07-31 19:44:21 -0400

    done task1: added entry page url and views

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def show(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        raise Http404(f"We are enable to to found the article  '{title}'.")
+
+    markdowner = Markdown()
+    html_content = markdowner.convert(entry)
+
+    return render(request, "encyclopedia/wiki_show.html", {
+        "title": title,
+        "html_content": html_content,
+    })
