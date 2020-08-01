8a04ffdf06e9d2b7d0f4d14092420e15c31a8fc7
danielkanangila <daniel.kanangila@gmail.com>
2020-08-01 12:53:32 -0400

    task3: done with new page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,16 @@
 from django import forms
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import Http404
+from django.urls import reverse
 from markdown2 import Markdown
 
 from . import util
 
 
 class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Title")
-    content = forms.CharField(label="Markdown Content", widget=forms.Textarea)
+    title = forms.CharField(label="Title", required=True, min_length=2)
+    content = forms.CharField(label="Markdown Content",
+                              widget=forms.Textarea, required=True, min_length=2)
 
 
 def index(request):
@@ -33,8 +35,25 @@ def show(request, title):
 
 def create(request):
     if request.method == "POST":
-        pass
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            if util.get_entry(title):
+                return render(request, "encyclopedia/create.html", {
+                    "form": form,
+                    "error": f"Article '{title}' already exist."
+                })
+            util.save_entry(title, content)
+            return HttpResponseRedirect(reverse(
+                'show',
+                kwargs={"title": title}
+            ))
+        else:
+            return render(request, "encyclopedia/create.html", {
+                "form": form
+            })
     else:
         return render(request, "encyclopedia/create.html", {
-            "form": NewEntryForm()
+            "form": NewEntryForm(),
         })
